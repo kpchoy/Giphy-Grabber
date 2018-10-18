495bc198941e19998e58d54878476ffd530376dd
Kevin Choy <kevinchoy22@gmail.com>
2018-10-18 12:12:47 -0700

    provider and hashrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
 
+
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import * as gifActions from './actions/gif_actions';
 import configureStore from './store/store';
 
+
+const Root = ({store}) => (
+  <Provider store = {store}>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </Provider>
+)
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore()
+
   // window.fetchGifs = gifActions.fetchGifs;
-  const store = configureStore();
   window.getState = store.getState;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(<Root store={store}/>, root);
 
 
 })
